1359cf3845653bfb09a0fa3465326c7122dbfae7
Swati Solanki <swatisolanki547@gmail.com>
2026-07-24 14:17:28 +0530

    Update ForLoopEx2.java

--- java/ForLoopEx2.java
+++ java/ForLoopEx2.java
@@ -1,18 +1,18 @@
 public class ForLoopEx2 {
     public static void main(String[] args) {
 		for(int a=1;a<=5;) {
-			System.out.println("Swati Shimle");
+			System.out.println("Swati Solanki");
 			a++;
 		}
 		System.out.println("--------------");
 		int a=1;
 		for(;a<=5;) {
-			System.out.println("Swati Shimle");
+			System.out.println("Swati Solanki");
 			a++;
 		}
 		System.out.println("--------------");
 		for(;;) {  //Infinite Loop
-			System.out.println("Swati Shimle");
+			System.out.println("Swati Solanki");
 		}
 		//System.out.println("bye");//error
 	}
